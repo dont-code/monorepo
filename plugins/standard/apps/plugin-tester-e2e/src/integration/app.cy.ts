6fef6be4f464b9a7be4e65d35f528b31f455ba64
Gérard Collin <gerard.collin@appliweb.net>
2024-05-13 22:46:33 +0200

    Added test & e2e-test commands

--- plugins/standard/apps/plugin-tester-e2e/src/integration/app.cy.ts
+++ plugins/standard/apps/plugin-tester-e2e/src/integration/app.cy.ts
@@ -25,42 +25,5 @@ describe('Plugin tester', () => {
     getPageTitle ().should('contain.text','Debug Page');
   });
 
-  it('should load and reset apps', () => {
-
-    cy.intercept('GET','/assets/dev/templates.json').as('LoadTemplate');
-      // First load the Task Manager app
-    getSubMenuWithText('Dev').click();// Move to dev page
-    cy.wait('@LoadTemplate');
-    clickAutoComplete("template");
-    selectPopupChoiceWithText("Task Manager Application"); // Create Entity name
-    getSendButton().click();
-    getToolbar().should('contain.text','Task Manager');
-    getSubMenus().should('have.length', 5);
-    getSubMenuWithText( 'Task').click();
-    getPageTitle().should('contain.text',"Task");
-    getTableHeader('Name').should('contain.text', "Name");
-
-      // Then load the Note Editor app
-    getSubMenuWithText('Dev').click();// Move to dev page
-
-    clickAutoComplete("template");
-    selectPopupChoiceWithText("Note Editor Application"); // Create Entity name
-    getSendButton().click();
-    getToolbar().should('contain.text','Note Editor');
-    getSubMenus().should('have.length', 5);
-    getSubMenuWithText( 'Note').click();
-    getPageTitle().should('contain.text',"Note");
-    getTableHeader('Content').should('contain.text', "Content");
-
-      // Then reset the app
-    getSubMenuWithText('Dev').click();// Move to dev page
-
-    clickAutoComplete("template");
-    selectPopupChoiceWithText("Reset"); // Create Entity name
-    getSendButton().click();
-    getToolbar().should('contain.text','No Name');
-    getSubMenus().should('have.length', 4);
-
-  });
 
 });
