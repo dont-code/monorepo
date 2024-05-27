e2c76aec4f397b847c6534c2faa96b2b9488f537
Gérard Collin <gerard.collin@appliweb.net>
2024-05-27 18:32:33 +0200

    Fixed bad testing for Project name confirm

--- apps/ide-ui/apps/ide-ui-e2e/src/support/projects.po.ts
+++ apps/ide-ui/apps/ide-ui-e2e/src/support/projects.po.ts
@@ -1,5 +1,5 @@
 export const getCurrentProjectButtonWithText = (text:string) => cy.get(".project-list-action").get("p-button").contains(text);
 export const selectConfirmButtonWithText = (text:string) => cy.get(".p-confirm-popup-footer .p-button-label").contains(text);
 export const getProjectNameInput = () => cy.get("#project-name-edit");
-export const getProjectNameValidator = () => cy.get(".pi-check");
+export const getProjectNameValidator = () => cy.get("p-inplace button");
 export const getProjectActionWithText = (projectName:string, text:string) => cy.contains(".project-list-item", projectName).find(".project-list-action > p-button").contains(text);
